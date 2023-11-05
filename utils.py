29856b26bec23e21a826b754c99dcf4bad456cb0
Louis Ben Arous <louisbenarous@gmail.com>
2023-11-05 00:29:13 -0700

    Add RAG capability

--- utils.py
+++ utils.py
@@ -1,32 +1,85 @@
 import os
 import getpass
 import openai
-from typing import Optional
+from typing import Optional, List
 from functools import cache
+from langchain import hub
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough
+
 
 @cache
 def get_openai_api_key():
-    return getpass.getpass('Input OpenAI API key= ')
+    return getpass.getpass("Input OpenAI API key= ")
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY", get_openai_api_key())
 
 
 def get_openai_response(
-        system_prompt: str,
-        user_prompt: str,
-        model: str = "gpt-4",
-        temperature: Optional[float] = 1.0) -> str:
+    system_prompt: str,
+    user_prompt: str,
+    model: str = "gpt-4",
+    temperature: Optional[float] = 1.0,
+) -> str:
+    """
+    Basic query to OpenAPI
+    :return: OpenAI response
+    """
     response = openai.ChatCompletion.create(
         model=model,
         temperature=temperature,
         messages=[
             {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_prompt}
-        ]
+            {"role": "user", "content": user_prompt},
+        ],
     )
     return response.choices[0].message.content
 
-# todo: add function example
+
+def get_openai_response_from_url(
+    prompt: str,
+    url: str,
+    embedding_model: str = "text-embedding-ada-002",
+    llm_model="gpt-4",
+) -> str:
+    """
+    Ask question to OpenAPI regarding content in a given website
+    Following the steps described in https://python.langchain.com/docs/use_cases/question_answering/
+    :return: OpenAI response
+    """
+
+    # crawl url and prepare embedding inputs
+    loader = WebBaseLoader(url)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=0)
+    documents = text_splitter.split_documents(loader.load())
+
+    # define util we'll use to embed the documents.
+    embedder = OpenAIEmbeddings(
+        openai_api_key=openai.api_key,
+        model=embedding_model,
+    )
+
+    # define the vector database we'll use to store and retrieve the embeddings
+    vector_store = Chroma.from_documents(documents=documents, embedding=embedder)
+    retriever = vector_store.as_retriever()
+
+    # define the large language model we'll use to answer the prompt
+    llm = ChatOpenAI(openai_api_key=openai.api_key, model_name=llm_model)
+
+    # put it all together
+    rag_prompt = hub.pull("rlm/rag-prompt")
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()} | rag_prompt | llm
+    )
+
+    # ask prompt and return response
+    response = rag_chain.invoke(prompt)
+    return response.content
+
+
 # todo: add finetuning example
-# todo: add embedding example
