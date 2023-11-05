cfdaae95102f175efde7b7f32e9d8ccffd1f1828
Louis Ben Arous <louisbenarous@gmail.com>
2023-11-04 23:07:32 -0700

    Add temperature and types

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import getpass
 import openai
+from typing import Optional
 from functools import cache
 
 @cache
@@ -11,12 +12,21 @@ def get_openai_api_key():
 openai.api_key = os.getenv("OPENAI_API_KEY", get_openai_api_key())
 
 
-def get_openai_response(system_prompt, user_prompt, model="gpt-3.5-turbo"):
+def get_openai_response(
+        system_prompt: str,
+        user_prompt: str,
+        model: str = "gpt-4",
+        temperature: Optional[float] = 1.0) -> str:
     response = openai.ChatCompletion.create(
         model=model,
+        temperature=temperature,
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt}
         ]
     )
     return response.choices[0].message.content
+
+# todo: add function example
+# todo: add finetuning example
+# todo: add embedding example
